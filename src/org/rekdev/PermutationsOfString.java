6cad1231f269502cfb6678e07724387890655fb4
Bob Kuhar <robertkuhar@gmail.com>
2014-08-14 23:20:10 -0700

    Add null checks to Permutations

--- src/org/rekdev/PermutationsOfString.java
+++ src/org/rekdev/PermutationsOfString.java
@@ -9,6 +9,8 @@ public class PermutationsOfString {
     }
 
     static private List<String> permutation( String prefix, String s ) {
+        checkNotNull( s );
+        checkNotNull( prefix );
         List<String> permutations = new ArrayList<String>();
         int n = s.length();
         if ( n == 0 ) {
@@ -20,13 +22,19 @@ public class PermutationsOfString {
         } else {
             for ( int i = 0; i < n; i++ ) {
                 /*
-                 * We're picking one character off the front and adding it to
-                 * the prefix and recursively calling permutation with the s
-                 * without the character at the current position.
+                 * Pick one character off the front, add it to the prefix, and
+                 * recursively call permutation with the s without the character
+                 * at the current position.
                  */
                 permutations.addAll( permutation( prefix + s.charAt( i ), s.substring( 0, i ) + s.substring( i + 1, n ) ) );
             }
         }
         return permutations;
+    }
+
+    static private void checkNotNull( String s ) {
+        if ( s == null ) {
+            throw new IllegalArgumentException();
+        }
     }
 }
