6b36dc6357e18c1f38774f3ea8918b9b21ddb9ed
robertkuhar <robertkuhar@gmail.com>
2025-09-26 10:42:02 -0700

    Improve the JavaDoc for RemoveElement

--- src/main/java/org/rekdev/leetcode/RemoveElement.java
+++ src/main/java/org/rekdev/leetcode/RemoveElement.java
@@ -9,13 +9,14 @@ public class RemoveElement {
    * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
    * The order of the elements may be changed. Then return the number of elements in nums which are
    * not equal to val.
+   * </p>
    * <p>
    * Consider the number of elements in nums which are not equal to val be k, to get accepted, you
    * need to do the following things:
    * - Change the array nums such that the first k elements of nums contain the elements which are
    * not equal to val. The remaining elements of nums are not important as well as the size of nums.
    * - Return k.
-   * <p>
+   * </p>
    * Custom Judge:
    * <p>
    * The judge will test your solution with the following code:
@@ -34,25 +35,28 @@ public class RemoveElement {
    * }
    * </pre>
    * If all assertions pass, then your solution will be accepted.
+   * </p>
    * <p>
-   * Example 1:
-   * Input: nums = [3,2,2,3], val = 3
-   * Output: 2, nums = [2,2,_,_]
+   * Example 1:<br/>
+   * Input: nums = [3,2,2,3], val = 3<br/>
+   * Output: 2, nums = [2,2,_,_]<br/>
    * Explanation: Your function should return k = 2, with the first two elements of nums being 2.
    * It does not matter what you leave beyond the returned k (hence they are underscores).
+   * </p>
    * <p>
-   * Example 2:
-   * <p>
-   * Input: nums = [0,1,2,2,3,0,4,2], val = 2
-   * Output: 5, nums = [0,1,4,0,3,_,_,_]
+   * Example 2:<br/>
+   * Input: nums = [0,1,2,2,3,0,4,2], val = 2<br/>
+   * Output: 5, nums = [0,1,4,0,3,_,_,_]<br/>
    * Explanation: Your function should return k = 5, with the first five elements of nums
    * containing 0, 0, 1, 3, and 4. Note that the five elements can be returned in any order. It
    * does not matter what you leave beyond the returned k (hence they are underscores).
+   * </p>
    * <p>
-   * Constraints:
-   * - 0 <= nums.length <= 100
-   * - 0 <= nums[i] <= 50
-   * - 0 <= val <= 100
+   * Constraints:<br/>
+   * - 0 <= nums.length <= 100<br/>
+   * - 0 <= nums[i] <= 50<br/>
+   * - 0 <= val <= 100<br/>
+   * </p>
    */
   public int removeElement(int[] nums, int val) {
     int k = 0;
